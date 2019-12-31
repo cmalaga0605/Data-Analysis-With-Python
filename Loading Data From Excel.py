cdce0598f27049d9f4f39551ce1629c53a269369
Cristian Malaga <47251490+cmalaga0605@users.noreply.github.com>
2019-12-30 19:36:15 -0500

    Add files via upload

---
+++ Loading Data From Excel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+Location = 'Documents/Excel Reports/supermarket_sales -.xlsx' # Location of data on the machine
+df = pd.read_excel(Location) #Create Dataframe 
+df.head() #Print first 5 rows of data
+
+
+# In[ ]:
+
+
+
+
